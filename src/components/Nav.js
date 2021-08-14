f79a77cefbc42a2b3b4312a007044819c88d3bf2
Kyle Canoy <kcanoy@uw.edu>
2021-08-13 23:41:06 -0700

    create ternary operator for About page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,10 @@ export default function Nav({ currentPage, handlePageChange }) {
     <Navbar>
       <Container>
         <Nav className="me-auto">
-          <Nav.Link href="#about">About</Nav.Link>
+          {/* Check if current page is "About", if true set current page to 'nav-link-active', else set to 'nav-link' */}
+          <Nav.Link href="#about" onClick={() => handlePageChange("About")}>
+            About
+          </Nav.Link>
           <Nav.Link href="#portfolio">Portfolio</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
           <Nav.Link href="#resume">Resume</Nav.Link>

1a5980504e3da9d2c5ae8d2cdfd3ca392be4aecf
Kyle Canoy <kcanoy@uw.edu>
2021-08-14 09:09:54 -0700

    switch to regular bootstrap5.0 instead of react-bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 // Import functions from components
 import React, { useState } from "react";
-import Nav from "./Nav";
+import Navigation from "./Navigation";
 import About from "./About";
 import Portfolio from "./Portfolio";
 import Contact from "./Contact";
@@ -32,7 +32,10 @@ export default function Header() {
   return (
     <div>
       {/* We are passing the currentPage from state and the function to update it */}
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
     </div>

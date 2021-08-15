320c58e6f880e7d5b55a7c55b7fb0bb9205cfdac
Kyle Canoy <kcanoy@uw.edu>
2021-08-14 20:44:08 -0700

    update resume page layout

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,10 +5,10 @@ export default function Resume() {
     <main className="container-fluid">
       <h1>Resume</h1>
       <hr></hr>
-      <div className="mt-6">
+      <div>
         <h2>Jefferson Kyle Canoy</h2>
       </div>
-      <div className="mt-6">
+      <div>
         Full Stack Web Developer with a background in psychology and medicine to
         provide a unique approach on how to ensure those with disabilities are
         able to access all aspects of a website or software. Earned a
@@ -18,19 +18,31 @@ export default function Resume() {
         adaptability, and willingness to rise to the occasion.
       </div>
 
-      <div className="mt-6">
-        <a href="www.linkedin.com/in/jeffersonkylecanoy">
-          <img
-            src="https://image.flaticon.com/icons/png/32/174/174857.png"
-            alt="LinkedIn logo"
-          ></img>
-        </a>
-        <a href="https://github.com/jkcanoy">
-          <img
-            src="https://image.flaticon.com/icons/png/32/733/733609.png"
-            alt="GitHub logo"
-          ></img>
-        </a>
+      <div className="d-flex flex-row">
+        <div>
+          <a href="www.linkedin.com/in/jeffersonkylecanoy">
+            <img
+              src="https://image.flaticon.com/icons/png/32/174/174857.png"
+              alt="LinkedIn logo"
+            ></img>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/jkcanoy">
+            <img
+              src="https://image.flaticon.com/icons/png/32/733/733609.png"
+              alt="GitHub logo"
+            ></img>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://github.com/jkcanoy/react-portfolio/blob/main/src/components/Resume/JKC%20Resume.pdf"
+            download
+          >
+            View Resume
+          </a>
+        </div>
       </div>
     </main>
   );

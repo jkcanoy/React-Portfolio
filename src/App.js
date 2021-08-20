37befe3f1e70c6356bd3df9564cfa0c9a218ad70
Kyle Canoy <kcanoy@uw.edu>
2021-08-20 13:44:43 -0700

    try gh pages fix, update font colors to look better on mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { HashRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
-    <div className="d-flex">
-      <Header />
+    <HashRouter>
+      <div className="d-flex">
+        <Header />
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }

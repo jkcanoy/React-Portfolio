83069d32f7e49e8ada78dfdd185d056b9bb98a6a
Kyle Canoy <kcanoy@uw.edu>
2021-08-20 13:47:09 -0700

    try another gh pages fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <div className="d-flex">
         <Header />
 
